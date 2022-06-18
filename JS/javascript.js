af715ce388c3b58cd9275fa12d668c7f33495f5b
Luan Leal <luanlealboni@gmail.com>
2022-06-17 22:48:15 -0300

    virar cartas

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,4 +1,10 @@
-const deck = ['bobrossparrot.gif', 'bobrossparrot.gif', 'explodyparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif','fiestaparrot.gif', 'metalparrot.gif','metalparrot.gif'];
+/* const deck = ['bobrossparrot.gif', 'bobrossparrot.gif',
+            'explodyparrot.gif', 'explodyparrot.gif', 
+            'fiestaparrot.gif','fiestaparrot.gif', 
+            'metalparrot.gif','metalparrot.gif',
+            'revertitparrot.gif','revertitparrot.gif', 
+            'tripletsparrot.gif', 'tripletsparrot.gif',
+            'unicornparrot.gif', 'unicornparrot.gif' ];
 let qtdCartas;
 iniciaJogo();
 
@@ -32,10 +38,18 @@ function distribuiCartas() {
  cartasNoJogo.sort(misturar);
 
 for (let i = 0; i < qtdCartas; i ++) {
-    const cartasTemplate = `<div class="card">
+    const cartasTemplate = `<div class="card" onclick="viraCarta(this)">
     <div class="front-card face"> <img src="front.png" alt=""> </div>
     <div class="back-card face"> <img src=${cartasNoJogo[i]} alt=""> </div>
     </div>`;
     painel.innerHTML += cartasTemplate;
 }
+} */
+const cartas = document.querySelectorAll('.card')
+
+function viraCarta() {
+    this.classList.toggle('virada');
 }
+
+cartas.forEach(carta => carta.addEventListener('click',viraCarta))
+
